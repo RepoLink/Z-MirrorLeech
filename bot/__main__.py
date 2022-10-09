56e11dba5e51a30f69df2ae8874768f5a268ce95
RepoLink <92987939+RepoLink@users.noreply.github.com>
2022-10-09 09:12:54 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,10 +61,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Discussion Group", "https://t.me/drivetalk")
+    buttons.buildbutton("Bot Tutorials", "https://t.me/BotTutorialsMM")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
