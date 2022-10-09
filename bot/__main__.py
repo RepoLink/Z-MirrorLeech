97481cf446ac3ea362ec861d41de1af2b490c1b4
mirrorbotmm <mirrorbot.mm@gmail.com>
2022-10-09 09:04:12 +0630

    Z-MirrorLeech

---
+++ bot/__main__.py
@@ -0,0 +1,307 @@
+from signal import signal, SIGINT
+from os import path as ospath, remove as osremove, execl as osexecl
+from subprocess import run as srun, check_output
+from psutil import disk_usage, cpu_percent, swap_memory, cpu_count, virtual_memory, net_io_counters, boot_time
+from time import time
+from sys import executable
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CommandHandler
+from bot import bot, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, LOGGER, Interval, INCOMPLETE_TASK_NOTIFIER,\
+                DB_URI, app, main_loop, AUTHORIZED_CHATS, TITLE_NAME
+from .helper.ext_utils.fs_utils import start_cleanup, clean_all, exit_clean_up
+from .helper.ext_utils.telegraph_helper import telegraph
+from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
+from .helper.ext_utils.db_handler import DbManger
+from .helper.telegram_helper.bot_commands import BotCommands
+from .helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage, sendLogFile, auto_delete_message
+from .helper.telegram_helper.filters import CustomFilters
+from .helper.telegram_helper.button_build import ButtonMaker
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, count, leech_settings, search, rss, bt_select
+from threading import Thread
+
+def progress_bar(percentage):
+    p_used = '⬢'
+    p_total = '⬡'
+    if isinstance(percentage, str):
+        return 'NaN'
+    try:
+        percentage=int(percentage)
+    except:
+        percentage = 0
+    return ''.join(
+        p_used if i <= percentage // 10 else p_total for i in range(1, 11)
+    )
+
+def stats(update, context):
+    if ospath.exists('.git'):
+        last_commit = check_output(["git log -1 --date=short --pretty=format:'%cr \n<b>Version: </b> %cd'"], shell=True).decode()
+    else:
+        last_commit = 'No UPSTREAM_REPO'
+    currentTime = get_readable_time(time() - botStartTime)
+    total, used, free, disk= disk_usage('/')
+    total = get_readable_file_size(total)
+    used = get_readable_file_size(used)
+    free = get_readable_file_size(free)
+    sent = get_readable_file_size(net_io_counters().bytes_sent)
+    recv = get_readable_file_size(net_io_counters().bytes_recv)
+    cpuUsage = cpu_percent(interval=1)
+    memory = virtual_memory()
+    mem_p = memory.percent
+    stats = f'<b><i><u>{TITLE_NAME} Bot Statistics</u></i></b>\n\n'\
+            f'<b>CPU</b>:  {progress_bar(cpuUsage)} {cpuUsage}%\n' \
+            f'<b>RAM</b>: {progress_bar(mem_p)} {mem_p}%\n' \
+            f'<b>DISK</b>: {progress_bar(disk)} {disk}%\n\n' \
+            f'<b>Updated:</b> {last_commit}\n'\
+            f'<b>I am Working For:</b> <code>{currentTime}</code>\n\n'\
+            f'<b>Total Disk:</b> <code>{total}</code> [{disk}% In use]\n'\
+            f'<b>Used:</b> <code>{used}</code> | <b>Free:</b> <code>{free}</code>\n'\
+            f'<b>T-UL:</b> <code>{sent}</code> | <b>T-DL:</b> <code>{recv}</code>\n'
+    reply_message = sendMessage(stats, context.bot, update.message)
+    Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
+
+def start(update, context):
+    buttons = ButtonMaker()
+    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
+    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
+    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
+    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
+    if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
+        start_string = f'''
+Welcome | {TITLE_NAME} service is ready for you
+Type /{BotCommands.HelpCommand} to get a list of available commands
+'''
+        sendMarkup(start_string, context.bot, update.message, reply_markup)
+    else:
+        sendMarkup('Sorry, You cannot use me', context.bot, update.message, reply_markup)
+
+def restart(update, context):
+    restart_message = sendMessage("Restarting...", context.bot, update.message)
+    if Interval:
+        Interval.clear()
+    clean_all()
+    srun(["pkill", "-f", "gunicorn|aria2c|qbittorrent-nox"])
+    srun(["python3", "update.py"])
+    with open(".restartmsg", "w") as f:
+        f.truncate(0)
+        f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
+    osexecl(executable, executable, "-m", "bot")
+
+def ping(update, context):
+    start_time = int(round(time() * 1000))
+    reply = sendMessage("Starting Ping", context.bot, update.message)
+    end_time = int(round(time() * 1000))
+    editMessage(f'{end_time - start_time} ms', reply)
+
+def log(update, context):
+    sendLogFile(context.bot, update.message)
+
+help_string_telegraph = f'''<br>
+<b>/{BotCommands.HelpCommand}</b>: To get this message
+<br><br>
+<b>/{BotCommands.MirrorCommand}</b> [download_url][magnet_link]: Start mirroring to Google Drive. Send <b>/{BotCommands.MirrorCommand}</b> for more help
+<br><br>
+<b>/{BotCommands.ZipMirrorCommand}</b> [download_url][magnet_link]: Start mirroring and upload the file/folder compressed with zip extension
+<br><br>
+<b>/{BotCommands.UnzipMirrorCommand}</b> [download_url][magnet_link]: Start mirroring and upload the file/folder extracted from any archive extension
+<br><br>
+<b>/{BotCommands.QbMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start Mirroring using qBittorrent. Send <b>/{BotCommands.QbMirrorCommand}</b> for more help
+<br><br>
+<b>/{BotCommands.QbZipMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start mirroring using qBittorrent and upload the file/folder compressed with zip extension
+<br><br>
+<b>/{BotCommands.QbUnzipMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start mirroring using qBittorrent and upload the file/folder extracted from any archive extension
+<br><br>
+<b>/{BotCommands.LeechCommand}</b> [download_url][magnet_link]: Start leeching to Telegram, Use <b>/{BotCommands.LeechCommand} s</b> to select files before leeching
+<br><br>
+<b>/{BotCommands.ZipLeechCommand}</b> [download_url][magnet_link]: Start leeching to Telegram and upload the file/folder compressed with zip extension
+<br><br>
+<b>/{BotCommands.UnzipLeechCommand}</b> [download_url][magnet_link][torent_file]: Start leeching to Telegram and upload the file/folder extracted from any archive extension
+<br><br>
+<b>/{BotCommands.QbLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start leeching to Telegram using qBittorrent, Use <b>/{BotCommands.QbLeechCommand} s</b> to select files before leeching
+<br><br>
+<b>/{BotCommands.QbZipLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start leeching to Telegram using qBittorrent and upload the file/folder compressed with zip extension
+<br><br>
+<b>/{BotCommands.QbUnzipLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start leeching to Telegram using qBittorrent and upload the file/folder extracted from any archive extension
+<br><br>
+<b>/{BotCommands.CloneCommand}</b> [drive_url][gdtot_url]: Copy file/folder to Google Drive
+<br><br>
+<b>/{BotCommands.CountCommand}</b> [drive_url][gdtot_url]: Count file/folder of Google Drive
+<br><br>
+<b>/{BotCommands.DeleteCommand}</b> [drive_url]: Delete file/folder from Google Drive (Only Owner & Sudo)
+<br><br>
+<b>/{BotCommands.WatchCommand}</b> [yt-dlp supported link]: Mirror yt-dlp supported link. Send <b>/{BotCommands.WatchCommand}</b> for more help
+<br><br>
+<b>/{BotCommands.ZipWatchCommand}</b> [yt-dlp supported link]: Mirror yt-dlp supported link as zip
+<br><br>
+<b>/{BotCommands.LeechWatchCommand}</b> [yt-dlp supported link]: Leech yt-dlp supported link
+<br><br>
+<b>/{BotCommands.LeechZipWatchCommand}</b> [yt-dlp supported link]: Leech yt-dlp supported link as zip
+<br><br>
+<b>/{BotCommands.LeechSetCommand}</b>: Leech settings
+<br><br>
+<b>/{BotCommands.SetThumbCommand}</b>: Reply photo to set it as Thumbnail
+<br><br>
+<b>/{BotCommands.BtSelectCommand}</b>: Reply to an active /cmd which was used to start the bt-download or add gid along with cmd. This command mainly for selection incase you decided to select files from already added torrent. But you can always use /cmd with arg `s` to select files before download start
+<br><br>
+<b>/{BotCommands.RssListCommand}</b>: List all subscribed rss feed info
+<br><br>
+<b>/{BotCommands.RssGetCommand}</b>: [Title] [Number](last N links): Force fetch last N links
+<br><br>
+<b>/{BotCommands.RssSubCommand}</b>: [Title] [Rss Link] f: [filter]: Subscribe new rss feed
+<br><br>
+<b>/{BotCommands.RssUnSubCommand}</b>: [Title]: Unubscribe rss feed by title
+<br><br>
+<b>/{BotCommands.RssSettingsCommand}</b>: Rss Settings
+<br><br>
+<b>/{BotCommands.CancelMirror}</b>: Reply to the message by which the download was initiated and that download will be cancelled
+<br><br>
+<b>/{BotCommands.CancelAllCommand}</b>: Cancel all downloading tasks
+<br><br>
+<b>/{BotCommands.ListCommand}</b> [query]: Search in Google Drive(s)
+<br><br>
+<b>/{BotCommands.SearchCommand}</b> [query]: Search for torrents with API
+<br>sites: <code>rarbg, 1337x, yts, etzv, tgx, torlock, piratebay, nyaasi, ettv</code><br><br>
+<b>/{BotCommands.StatusCommand}</b>: Shows a status of all the downloads
+<br><br>
+<b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
+'''
+
+help = telegraph.create_page(
+        title= f'{TITLE_NAME} Help',
+        content=help_string_telegraph,
+    )["path"]
+
+help_string = f'''
+/{BotCommands.PingCommand}: Check how long it takes to Ping the Bot
+
+/{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
+
+/{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
+
+/{BotCommands.AuthorizedUsersCommand}: Show authorized users (Only Owner & Sudo)
+
+/{BotCommands.AddSudoCommand}: Add sudo user (Only Owner)
+
+/{BotCommands.RmSudoCommand}: Remove sudo users (Only Owner)
+
+/{BotCommands.RestartCommand}: Restart and update the bot
+
+/{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
+
+/{BotCommands.ShellCommand}: Run commands in Shell (Only Owner)
+
+/{BotCommands.ExecHelpCommand}: Get help for Executor module (Only Owner)
+'''
+
+def bot_help(update, context):
+    button = ButtonMaker()
+    button.buildbutton("Other Commands", f"https://graph.org/{help}")
+    reply_markup = InlineKeyboardMarkup(button.build_menu(1))
+    sendMarkup(help_string, context.bot, update.message, reply_markup)
+
+botcmds = [
+
+        (f'{BotCommands.MirrorCommand}', 'Mirror'),
+        (f'{BotCommands.ZipMirrorCommand}','Mirror and upload as zip'),
+        (f'{BotCommands.UnzipMirrorCommand}','Mirror and extract files'),
+        (f'{BotCommands.QbMirrorCommand}','Mirror torrent using qBittorrent'),
+        (f'{BotCommands.QbZipMirrorCommand}','Mirror torrent and upload as zip using qb'),
+        (f'{BotCommands.QbUnzipMirrorCommand}','Mirror torrent and extract files using qb'),
+        (f'{BotCommands.WatchCommand}','Mirror yt-dlp supported link'),
+        (f'{BotCommands.ZipWatchCommand}','Mirror yt-dlp supported link as zip'),
+        (f'{BotCommands.CloneCommand}','Copy file/folder to Drive'),
+        (f'{BotCommands.LeechCommand}','Leech'),
+        (f'{BotCommands.ZipLeechCommand}','Leech and upload as zip'),
+        (f'{BotCommands.UnzipLeechCommand}','Leech and extract files'),
+        (f'{BotCommands.QbLeechCommand}','Leech torrent using qBittorrent'),
+        (f'{BotCommands.QbZipLeechCommand}','Leech torrent and upload as zip using qb'),
+        (f'{BotCommands.QbUnzipLeechCommand}','Leech torrent and extract using qb'),
+        (f'{BotCommands.LeechWatchCommand}','Leech yt-dlp supported link'),
+        (f'{BotCommands.LeechZipWatchCommand}','Leech yt-dlp supported link as zip'),
+        (f'{BotCommands.CountCommand}','Count file/folder of Drive'),
+        (f'{BotCommands.DeleteCommand}','Delete file/folder from Drive'),
+        (f'{BotCommands.CancelMirror}','Cancel a task'),
+        (f'{BotCommands.CancelAllCommand}','Cancel all downloading tasks'),
+        (f'{BotCommands.ListCommand}','Search in Drive'),
+        (f'{BotCommands.LeechSetCommand}','Leech settings'),
+        (f'{BotCommands.SetThumbCommand}','Set thumbnail'),
+        (f'{BotCommands.StatusCommand}','Get mirror status message'),
+        (f'{BotCommands.StatsCommand}','Bot usage stats'),
+        (f'{BotCommands.PingCommand}','Ping the bot'),
+        (f'{BotCommands.RestartCommand}','Restart the bot'),
+        (f'{BotCommands.LogCommand}','Get the bot Log'),
+        (f'{BotCommands.HelpCommand}','Get detailed help')
+    ]
+
+def main():
+    # bot.set_my_commands(botcmds)
+    start_cleanup()
+    notifier_dict = None
+    if INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
+        if notifier_dict := DbManger().get_incomplete_tasks():
+            for cid, data in notifier_dict.items():
+                if ospath.isfile(".restartmsg"):
+                    with open(".restartmsg") as f:
+                        chat_id, msg_id = map(int, f)
+                    msg = 'Restarted successfully!'
+                else:
+                    msg = 'Bot Restarted!'
+                for tag, links in data.items():
+                     msg += f"\nIncomplete Tasks List:\n\n{tag}: "
+                     for index, link in enumerate(links, start=1):
+                         msg += f" <a href='{link}'>{index}</a> \n"
+                         if len(msg.encode()) > 4000:
+                             if 'Restarted successfully!' in msg and cid == chat_id:
+                                 bot.editMessageText(msg, chat_id, msg_id, parse_mode='HTMl', disable_web_page_preview=True)
+                                 osremove(".restartmsg")
+                             else:
+                                 try:
+                                     bot.sendMessage(cid, msg, 'HTML', disable_web_page_preview=True)
+                                 except Exception as e:
+                                     LOGGER.error(e)
+                             msg = ''
+                if 'Restarted successfully!' in msg and cid == chat_id:
+                     bot.editMessageText(msg, chat_id, msg_id, parse_mode='HTMl', disable_web_page_preview=True)
+                     osremove(".restartmsg")
+                else:
+                    try:
+                        bot.sendMessage(cid, msg, 'HTML', disable_web_page_preview=True)
+                    except Exception as e:
+                        LOGGER.error(e)
+
+    if ospath.isfile(".restartmsg"):
+        with open(".restartmsg") as f:
+            chat_id, msg_id = map(int, f)
+        bot.edit_message_text("Restarted successfully!", chat_id, msg_id)
+        osremove(".restartmsg")
+    elif not notifier_dict and AUTHORIZED_CHATS:
+        for id_ in AUTHORIZED_CHATS:
+            try:
+                bot.sendMessage(id_, "Bot Restarted!", 'HTML')
+            except Exception as e:
+                LOGGER.error(e)
+
+    start_handler = CommandHandler(BotCommands.StartCommand, start, run_async=True)
+    ping_handler = CommandHandler(BotCommands.PingCommand, ping,
+                                  filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    restart_handler = CommandHandler(BotCommands.RestartCommand, restart,
+                                     filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    help_handler = CommandHandler(BotCommands.HelpCommand,
+                                  bot_help, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    stats_handler = CommandHandler(BotCommands.StatsCommand,
+                                   stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(ping_handler)
+    dispatcher.add_handler(restart_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(stats_handler)
+    dispatcher.add_handler(log_handler)
+    updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
+    LOGGER.info("Congratulations, Bot Started Sucessfully !")
+    signal(SIGINT, exit_clean_up)
+
+app.start()
+main()
+
+main_loop.run_forever()
---
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -0,0 +1,143 @@
+from time import sleep
+from bot import aria2, download_dict_lock, download_dict, STOP_DUPLICATE, TORRENT_DIRECT_LIMIT, ZIP_UNZIP_LIMIT, LEECH_LIMIT, LOGGER, STORAGE_THRESHOLD, BASE_URL
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+from bot.helper.ext_utils.bot_utils import is_magnet, getDownloadByGid, new_thread, get_readable_file_size, bt_selection_buttons
+from bot.helper.mirror_utils.status_utils.aria_download_status import AriaDownloadStatus
+from bot.helper.telegram_helper.message_utils import sendMarkup, sendStatusMessage, sendMessage, deleteMessage
+from bot.helper.ext_utils.fs_utils import get_base_name, check_storage_threshold, clean_unwanted
+
+@new_thread
+def __onDownloadStarted(api, gid):
+    download = api.get_download(gid)
+    if download.is_metadata:
+        LOGGER.info(f'onDownloadStarted: {gid} Metadata')
+        dl = getDownloadByGid(gid)
+        if dl.listener().select:
+            metamsg = "Downloading Metadata, wait then you can select files. Use torrent file to avoid this wait."
+            meta = sendMessage(metamsg, dl.listener().bot, dl.listener().message)
+            while True:
+                download = api.get_download(gid)
+                if download.followed_by_ids:
+                    deleteMessage(dl.listener().bot, meta)
+                    break
+                sleep(1)
+        return
+    else:
+        LOGGER.info(f'onDownloadStarted: {gid}')
+    try:
+        if any([STOP_DUPLICATE, TORRENT_DIRECT_LIMIT, ZIP_UNZIP_LIMIT, LEECH_LIMIT, STORAGE_THRESHOLD]):
+            download = api.get_download(gid)
+            if not download.is_torrent:
+                sleep(3)
+                download = api.get_download(gid)
+            dl = getDownloadByGid(gid)
+            if not dl:
+                return
+            if STOP_DUPLICATE and not dl.listener().isLeech:
+                LOGGER.info('Checking File/Folder if already in Drive...')
+                sname = download.name
+                if dl.listener().isZip:
+                    sname = f"{sname}.zip"
+                elif dl.listener().extract:
+                    try:
+                        sname = get_base_name(sname)
+                    except:
+                        sname = None
+                if sname is not None:
+                    smsg, button = GoogleDriveHelper().drive_list(sname, True)
+                    if smsg:
+                        dl.listener().onDownloadError('Someone already mirrored it for you !\n\n')
+                        api.remove([download], force=True, files=True)
+                        return sendMarkup("Here you go:", dl.listener().bot, dl.listener().message, button)
+            if any([ZIP_UNZIP_LIMIT, LEECH_LIMIT, TORRENT_DIRECT_LIMIT, STORAGE_THRESHOLD]):
+                sleep(1)
+                limit = None
+                size = download.total_length
+                arch = any([dl.listener().isZip, dl.listener().isLeech, dl.listener().extract])
+                if STORAGE_THRESHOLD is not None:
+                    acpt = check_storage_threshold(size, arch, True)
+                    # True if files allocated, if allocation disabled remove True arg
+                    if not acpt:
+                        msg = f'You must leave {STORAGE_THRESHOLD}GB free storage.'
+                        msg += f'\nYour File/Folder size is {get_readable_file_size(size)}'
+                        dl.listener().onDownloadError(msg)
+                        return api.remove([download], force=True, files=True)
+                if ZIP_UNZIP_LIMIT is not None and arch:
+                    mssg = f'Zip/Unzip limit is {ZIP_UNZIP_LIMIT}GB'
+                    limit = ZIP_UNZIP_LIMIT
+                if LEECH_LIMIT is not None and dl.listener().isLeech:
+                    mssg = f'Leech limit is {LEECH_LIMIT}GB'
+                    limit = LEECH_LIMIT
+                elif TORRENT_DIRECT_LIMIT is not None:
+                    mssg = f'Torrent/Direct limit is {TORRENT_DIRECT_LIMIT}GB'
+                    limit = TORRENT_DIRECT_LIMIT
+                if limit is not None:
+                    LOGGER.info('Checking File/Folder Size...')
+                    if size > limit * 1024**3:
+                        dl.listener().onDownloadError(f'{mssg}.\nYour File/Folder size is {get_readable_file_size(size)}')
+                        return api.remove([download], force=True, files=True)
+    except Exception as e:
+        LOGGER.error(f"{e} onDownloadStart: {gid} stop duplicate and size check didn't pass")
+
+@new_thread
+def __onDownloadComplete(api, gid):
+    download = api.get_download(gid)
+    if download.followed_by_ids:
+        new_gid = download.followed_by_ids[0]
+        LOGGER.info(f'Gid changed from {gid} to {new_gid}')
+        if BASE_URL is not None:
+            dl = getDownloadByGid(new_gid)
+            if dl and dl.listener().select:
+                api.client.force_pause(new_gid)
+                SBUTTONS = bt_selection_buttons(new_gid)
+                msg = "Your download paused. Choose files then press Done Selecting button to start downloading."
+                sendMarkup(msg, dl.listener().bot, dl.listener().message, SBUTTONS)
+    elif dl := getDownloadByGid(gid):
+        LOGGER.info(f"onDownloadComplete: {gid}")
+        if dl.listener().select:
+            clean_unwanted(dl.path())
+        dl.listener().onDownloadComplete()
+
+@new_thread
+def __onDownloadStopped(api, gid):
+    sleep(6)
+    if dl := getDownloadByGid(gid):
+        dl.listener().onDownloadError('Dead torrent!')
+
+@new_thread
+def __onDownloadError(api, gid):
+    LOGGER.info(f"onDownloadError: {gid}")
+    try:
+        download = api.get_download(gid)
+        error = download.error_message
+        LOGGER.info(f"Download Error: {error}")
+    except:
+        pass
+    if dl:
+        dl.listener().onDownloadError(error)
+
+def start_listener():
+    aria2.listen_to_notifications(threaded=True,
+                                  on_download_start=__onDownloadStarted,
+                                  on_download_error=__onDownloadError,
+                                  on_download_stop=__onDownloadStopped,
+                                  on_download_complete=__onDownloadComplete,
+                                  timeout=20)
+
+def add_aria2c_download(link: str, path, listener, filename, auth, select):
+    if is_magnet(link):
+        download = aria2.add_magnet(link, {'dir': path})
+    else:
+        download = aria2.add_uris([link], {'dir': path, 'out': filename, 'header': f"authorization: {auth}"})
+    if download.error_message:
+        error = str(download.error_message).replace('<', ' ').replace('>', ' ')
+        LOGGER.info(f"Download Error: {error}")
+        return sendMessage(error, listener.bot, listener.message)
+    with download_dict_lock:
+        download_dict[listener.uid] = AriaDownloadStatus(download.gid, listener)
+        LOGGER.info(f"Download Started with area2: {download.gid} DIR: {download.dir} ")
+    listener.onDownloadStart()
+    if not select:
+        sendStatusMessage(listener.message, listener.bot)
+
+start_listener()
